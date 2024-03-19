de13044fd0ee5f3545dd4a41ac915c7d653cebe9
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2024-03-19 10:52:04 -0400

    test sync merge

---
+++ private/src/Marc/Tests/ForkSyncTest.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * 420DW3_07278_Project_Fork ForkSyncTest.php
+ * 
+ * @author Marc-Eric Boury (MEbou)
+ * @since 2024-03-19
+ * (c) Copyright 2024 Marc-Eric Boury 
+ */
+
+namespace Marc\Tests;
+
+/**
+ * TODO: Class documentation
+ *
+ * @author Marc-Eric Boury
+ * @since  2024-03-19
+ */
+class ForkSyncTest {
+    
+}
